bbaa7638d0bd54b72d4ec5c0a37f819a30c6f0d0
Henry Schreiner <hschrein@cern.ch>
2017-03-30 15:51:00 -0400

    Adding correct location for CLI11

--- AddCLI.cmake
+++ AddCLI.cmake
@@ -2,7 +2,7 @@
 include(DownloadProject)
 message(STATUS "Downloading CLI if needed")
 download_project(PROJ                cli
-                 GIT_REPOSITORY      https://github.com/henryiii/CLI11.git
+                 GIT_REPOSITORY      https://github.com/CLIUtils/CLI11.git
 		         GIT_TAG             master
                  UPDATE_DISCONNECTED 1
                  QUIET
