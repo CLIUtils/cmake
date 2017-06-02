d1f6b781603482cd49e21585f7e03ed188605f26
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-06-02 09:04:20 -0400

    Cleaner IDE folder structure for GTest

--- AddGoogleTest.cmake
+++ AddGoogleTest.cmake
@@ -6,8 +6,6 @@
 #
 set(UPDATE_DISCONNECTED_IF_AVAILABLE "UPDATE_DISCONNECTED 1")
 
-message(STATUS "Downloading GoogleTest if needed")
-
 include(DownloadProject)
 download_project(PROJ                googletest
                  GIT_REPOSITORY      https://github.com/google/googletest.git
@@ -19,15 +17,6 @@ download_project(PROJ                googletest
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 add_subdirectory(${googletest_SOURCE_DIR} ${googletest_SOURCE_DIR})
 
-#mark_as_advanced(
-#    gtest_build_samples
-#    gtest_build_tests
-#    gtest_disable_pthreads
-#    gtest_force_shared_crt
-#    gtest_hide_internal_symbols
-#    BUILD_SHARED_LIBS
-#)
-
 if (CMAKE_CONFIGURATION_TYPES)
     add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND} 
         --force-new-ctest-process --output-on-failure 
@@ -36,6 +25,7 @@ else()
     add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND} 
         --force-new-ctest-process --output-on-failure)
 endif()
+set_target_properties(check PROPERTIES FOLDER "Scripts")
 
 #include_directories(${gtest_SOURCE_DIR}/include)
 
@@ -46,6 +36,7 @@ endif()
 macro(add_gtest TESTNAME)
     target_link_libraries(${TESTNAME} PUBLIC gtest gmock gtest_main)
     add_test(${TESTNAME} ${TESTNAME})
+    set_target_properties(${TESTNAME} PROPERTIES FOLDER "Tests")
 endmacro()
 
 mark_as_advanced(
@@ -57,3 +48,5 @@ gtest_force_shared_crt
 gtest_hide_internal_symbols
 )
 
+set_target_properties(gtest gtest_main gmock gmock_main
+    PROPERTIES FOLDER "Extern")
