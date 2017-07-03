ec4715bed4abdb9b75741aa30a5f62580e8b7243
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-07-03 13:14:41 -0400

    Using built in ROOT utils if available

--- FindROOT.cmake
+++ FindROOT.cmake
@@ -134,106 +134,111 @@ find_program(ROOTCINT_EXECUTABLE rootcint PATHS $ENV{ROOTSYS}/bin)
 find_program(GENREFLEX_EXECUTABLE genreflex PATHS $ENV{ROOTSYS}/bin)
 find_package(GCCXML)
 
+if(EXISTS "${ROOTSYS}/cmake/modules/RootNewMacros.cmake")
+    set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} $ENV{ROOTSYS}/cmake/modules)
+    include(RootNewMacros)
+else()
 #----------------------------------------------------------------------------
 # function ROOT_GENERATE_DICTIONARY( dictionary
 #                                    header1 header2 ...
 #                                    LINKDEF linkdef1 ...
 #                                    OPTIONS opt1...)
-function(ROOT_GENERATE_DICTIONARY dictionary)
-  CMAKE_PARSE_ARGUMENTS(ARG "" "" "LINKDEF;OPTIONS" "" ${ARGN})
-  #---Get the list of include directories------------------
-  get_directory_property(incdirs INCLUDE_DIRECTORIES)
-  set(includedirs)
-  foreach( d ${incdirs})
-     set(includedirs ${includedirs} -I${d})
-  endforeach()
-  #---Get the list of header files-------------------------
-  set(headerfiles)
-  foreach(fp ${ARG_UNPARSED_ARGUMENTS})
-    if(${fp} MATCHES "[*?]") # Is this header a globbing expression?
-      file(GLOB files ${fp})
-      foreach(f ${files})
-        if(NOT f MATCHES LinkDef) # skip LinkDefs from globbing result
-          set(headerfiles ${headerfiles} ${f})
+    function(ROOT_GENERATE_DICTIONARY dictionary)
+      CMAKE_PARSE_ARGUMENTS(ARG "" "" "LINKDEF;OPTIONS" "" ${ARGN})
+      #---Get the list of include directories------------------
+      get_directory_property(incdirs INCLUDE_DIRECTORIES)
+      set(includedirs)
+      foreach( d ${incdirs})
+         set(includedirs ${includedirs} -I${d})
+      endforeach()
+      #---Get the list of header files-------------------------
+      set(headerfiles)
+      foreach(fp ${ARG_UNPARSED_ARGUMENTS})
+        if(${fp} MATCHES "[*?]") # Is this header a globbing expression?
+          file(GLOB files ${fp})
+          foreach(f ${files})
+            if(NOT f MATCHES LinkDef) # skip LinkDefs from globbing result
+              set(headerfiles ${headerfiles} ${f})
+            endif()
+          endforeach()
+        else()
+          find_file(headerFile ${fp} PATHS ${incdirs})
+          set(headerfiles ${headerfiles} ${headerFile})
+          unset(headerFile CACHE)
         endif()
       endforeach()
-    else()
-      find_file(headerFile ${fp} PATHS ${incdirs})
-      set(headerfiles ${headerfiles} ${headerFile})
-      unset(headerFile CACHE)
-    endif()
-  endforeach()
-  #---Get LinkDef.h file------------------------------------
-  set(linkdefs)
-  foreach( f ${ARG_LINKDEF})
-    find_file(linkFile ${f} PATHS ${incdirs})
-    set(linkdefs ${linkdefs} ${linkFile})
-    unset(linkFile CACHE)
-  endforeach()
-  #---call rootcint------------------------------------------
-  add_custom_command(OUTPUT ${dictionary}.cxx ${dictionary}.h
-                     COMMAND ${ROOTCINT_EXECUTABLE} -cint -f  ${dictionary}.cxx
-                                          -c ${ARG_OPTIONS} ${includedirs} ${headerfiles} ${linkdefs}
-                     DEPENDS ${headerfiles} ${linkdefs} VERBATIM)
-endfunction()
+      #---Get LinkDef.h file------------------------------------
+      set(linkdefs)
+      foreach( f ${ARG_LINKDEF})
+        find_file(linkFile ${f} PATHS ${incdirs})
+        set(linkdefs ${linkdefs} ${linkFile})
+        unset(linkFile CACHE)
+      endforeach()
+      #---call rootcint------------------------------------------
+      add_custom_command(OUTPUT ${dictionary}.cxx ${dictionary}.h
+                         COMMAND ${ROOTCINT_EXECUTABLE} -cint -f  ${dictionary}.cxx
+                                              -c ${ARG_OPTIONS} ${includedirs} ${headerfiles} ${linkdefs}
+                         DEPENDS ${headerfiles} ${linkdefs} VERBATIM)
+    endfunction()
 
 #----------------------------------------------------------------------------
 # function REFLEX_GENERATE_DICTIONARY(dictionary
 #                                     header1 header2 ...
 #                                     SELECTION selectionfile ...
 #                                     OPTIONS opt1...)
-function(REFLEX_GENERATE_DICTIONARY dictionary)
-  CMAKE_PARSE_ARGUMENTS(ARG "" "" "SELECTION;OPTIONS" "" ${ARGN})
-  #---Get the list of header files-------------------------
-  set(headerfiles)
-  foreach(fp ${ARG_UNPARSED_ARGUMENTS})
-    file(GLOB files ${fp})
-    if(files)
-      foreach(f ${files})
-        set(headerfiles ${headerfiles} ${f})
+    function(REFLEX_GENERATE_DICTIONARY dictionary)
+      CMAKE_PARSE_ARGUMENTS(ARG "" "" "SELECTION;OPTIONS" "" ${ARGN})
+      #---Get the list of header files-------------------------
+      set(headerfiles)
+      foreach(fp ${ARG_UNPARSED_ARGUMENTS})
+        file(GLOB files ${fp})
+        if(files)
+          foreach(f ${files})
+            set(headerfiles ${headerfiles} ${f})
+          endforeach()
+        else()
+          set(headerfiles ${headerfiles} ${fp})
+        endif()
       endforeach()
-    else()
-      set(headerfiles ${headerfiles} ${fp})
-    endif()
-  endforeach()
-  #---Get Selection file------------------------------------
-  if(IS_ABSOLUTE ${ARG_SELECTION})
-    set(selectionfile ${ARG_SELECTION})
-  else()
-    set(selectionfile ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_SELECTION})
-  endif()
-  #---Get the list of include directories------------------
-  get_directory_property(incdirs INCLUDE_DIRECTORIES)
-  set(includedirs)
-  foreach( d ${incdirs})
-    set(includedirs ${includedirs} -I${d})
-  endforeach()
-  #---Get preprocessor definitions--------------------------
-  get_directory_property(defs COMPILE_DEFINITIONS)
-  foreach( d ${defs})
-   set(definitions ${definitions} -D${d})
-  endforeach()
-  #---Nanes and others---------------------------------------
-  set(gensrcdict ${dictionary}.cpp)
-  if(MSVC)
-    set(gccxmlopts "--gccxmlopt=\"--gccxml-compiler cl\"")
-  else()
-    #set(gccxmlopts "--gccxmlopt=\'--gccxml-cxxflags -m64 \'")
-    set(gccxmlopts)
-  endif()
-  #set(rootmapname ${dictionary}Dict.rootmap)
-  #set(rootmapopts --rootmap=${rootmapname} --rootmap-lib=${libprefix}${dictionary}Dict)
-  #---Check GCCXML and get path-----------------------------
-  if(GCCXML)
-    get_filename_component(gccxmlpath ${GCCXML} PATH)
-  else()
-    message(WARNING "GCCXML not found. Install and setup your environment to find 'gccxml' executable")
-  endif()
-  #---Actual command----------------------------------------
-  add_custom_command(OUTPUT ${gensrcdict} ${rootmapname}
-                     COMMAND ${GENREFLEX_EXECUTABLE} ${headerfiles} -o ${gensrcdict} ${gccxmlopts} ${rootmapopts} --select=${selectionfile}
-                             --gccxmlpath=${gccxmlpath} ${ARG_OPTIONS} ${includedirs} ${definitions}
-                     DEPENDS ${headerfiles} ${selectionfile})
-endfunction()
+      #---Get Selection file------------------------------------
+      if(IS_ABSOLUTE ${ARG_SELECTION})
+        set(selectionfile ${ARG_SELECTION})
+      else()
+        set(selectionfile ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_SELECTION})
+      endif()
+      #---Get the list of include directories------------------
+      get_directory_property(incdirs INCLUDE_DIRECTORIES)
+      set(includedirs)
+      foreach( d ${incdirs})
+        set(includedirs ${includedirs} -I${d})
+      endforeach()
+      #---Get preprocessor definitions--------------------------
+      get_directory_property(defs COMPILE_DEFINITIONS)
+      foreach( d ${defs})
+       set(definitions ${definitions} -D${d})
+      endforeach()
+      #---Nanes and others---------------------------------------
+      set(gensrcdict ${dictionary}.cpp)
+      if(MSVC)
+        set(gccxmlopts "--gccxmlopt=\"--gccxml-compiler cl\"")
+      else()
+        #set(gccxmlopts "--gccxmlopt=\'--gccxml-cxxflags -m64 \'")
+        set(gccxmlopts)
+      endif()
+      #set(rootmapname ${dictionary}Dict.rootmap)
+      #set(rootmapopts --rootmap=${rootmapname} --rootmap-lib=${libprefix}${dictionary}Dict)
+      #---Check GCCXML and get path-----------------------------
+      if(GCCXML)
+        get_filename_component(gccxmlpath ${GCCXML} PATH)
+      else()
+        message(WARNING "GCCXML not found. Install and setup your environment to find 'gccxml' executable")
+      endif()
+      #---Actual command----------------------------------------
+      add_custom_command(OUTPUT ${gensrcdict} ${rootmapname}
+                         COMMAND ${GENREFLEX_EXECUTABLE} ${headerfiles} -o ${gensrcdict} ${gccxmlopts} ${rootmapopts} --select=${selectionfile}
+                                 --gccxmlpath=${gccxmlpath} ${ARG_OPTIONS} ${includedirs} ${definitions}
+                         DEPENDS ${headerfiles} ${selectionfile})
+    endfunction()
+endif()
 
 mark_as_advanced(ROOTCINT_EXECUTABLE GENREFLEX_EXECUTABLE) 
