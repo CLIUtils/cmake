7a3e22a3af14de0191badfcf4bfb5e8772808454
Henry Schreiner <hschrein@cern.ch>
2017-10-27 12:16:23 -0400

    Better support for windows

--- CUDA.cmake
+++ CUDA.cmake
@@ -24,7 +24,7 @@ function(CUDA_CONVERT_FLAGS EXISTING_TARGET)
     if(NOT "${old_flags}" STREQUAL "")
         string(REPLACE ";" "," CUDA_flags "${old_flags}")
         set_property(TARGET ${EXISTING_TARGET} PROPERTY INTERFACE_COMPILE_OPTIONS
-            "$<$<BUILD_INTERFACE:$<COMPILE_LANGUAGE:CXX>>:${old_flags}>$<$<BUILD_INTERFACE:$<COMPILE_LANGUAGE:CUDA>>:-Xcompiler=${CUDA_flags}>"
+            "$<$<BUILD_INTERFACE:$<NOT:$<COMPILE_LANGUAGE:CUDA>>>:${old_flags}>$<$<BUILD_INTERFACE:$<COMPILE_LANGUAGE:CUDA>>:-Xcompiler=${CUDA_flags}>"
             )
     endif()
 endfunction()
@@ -33,7 +33,7 @@ function(CUDA_PROTECT_FLAGS EXISTING_TARGET)
     get_property(old_flags TARGET ${EXISTING_TARGET} PROPERTY INTERFACE_COMPILE_OPTIONS)
     if(NOT "${old_flags}" STREQUAL "")
         set_property(TARGET ${EXISTING_TARGET} PROPERTY INTERFACE_COMPILE_OPTIONS
-            "$<$<BUILD_INTERFACE:$<COMPILE_LANGUAGE:CXX>>:${old_flags}>"
+            "$<$<BUILD_INTERFACE:$<NOT:$<COMPILE_LANGUAGE:CUDA>>>:${old_flags}>"
             )
     endif()
 endfunction()
