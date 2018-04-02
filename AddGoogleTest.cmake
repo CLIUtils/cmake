4e52e4d0bc2e9fd27171926d0b5d9f396dd8637c
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-04-02 12:10:24 +0200

    Using FetchContent if avaiable

--- AddGoogleTest.cmake
+++ AddGoogleTest.cmake
@@ -4,20 +4,39 @@
 # gives output on failed tests without having to set an environment variable.
 #
 #
-set(UPDATE_DISCONNECTED_IF_AVAILABLE "UPDATE_DISCONNECTED 1")
-
-include(DownloadProject)
-download_project(PROJ                googletest
-                 GIT_REPOSITORY      https://github.com/google/googletest.git
-                 GIT_TAG             release-1.8.0
-                 UPDATE_DISCONNECTED 1
-                 QUIET
-)
-
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-add_subdirectory(${googletest_SOURCE_DIR} ${googletest_SOURCE_DIR})
 
-if (CMAKE_CONFIGURATION_TYPES)
+if(CMAKE_VERSION VERSION_LESS 3.11)
+    set(UPDATE_DISCONNECTED_IF_AVAILABLE "UPDATE_DISCONNECTED 1")
+
+    include(DownloadProject)
+    download_project(PROJ                googletest
+		     GIT_REPOSITORY      https://github.com/google/googletest.git
+		     GIT_TAG             release-1.8.0
+		     UPDATE_DISCONNECTED 1
+		     QUIET
+    )
+
+    # CMake warning suppression will not be needed in version 1.9
+    set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS 1 CACHE BOOL "")
+    add_subdirectory(${googletest_SOURCE_DIR} ${googletest_SOURCE_DIR} EXCLUDE_FROM_ALL)
+    unset(CMAKE_SUPPRESS_DEVELOPER_WARNINGS)
+else()
+    include(FetchContent)
+    FetchContent_Declare(googletest
+        GIT_REPOSITORY      https://github.com/google/googletest.git
+        GIT_TAG             release-1.8.0)
+    FetchContent_GetProperties(googletest)
+    if(NOT googletest_POPULATED)
+        FetchContent_Populate(googletest)
+        set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS 1 CACHE BOOL "")
+        add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR} EXCLUDE_FROM_ALL)
+        unset(CMAKE_SUPPRESS_DEVELOPER_WARNINGS)
+    endif()
+endif()
+
+
+if(CMAKE_CONFIGURATION_TYPES)
     add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND} 
         --force-new-ctest-process --output-on-failure 
         --build-config "$<CONFIGURATION>")
