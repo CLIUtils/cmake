9a7cb2be57b02054ec5b51e492898fb13e8c4b98
Lorenzo Principe <28869147+lorenzomag@users.noreply.github.com>
2021-07-21 18:37:25 +0100

    Latest release of GoogleTest for modern Cmake (#2)
    Using release 1.8.0 produces an annoying amount of deprecation warnings  at the moment for current versions of CMake.

--- AddGoogleTest.cmake
+++ AddGoogleTest.cmake
@@ -24,8 +24,7 @@ if(CMAKE_VERSION VERSION_LESS 3.11)
 else()
     include(FetchContent)
     FetchContent_Declare(googletest
-        GIT_REPOSITORY      https://github.com/google/googletest.git
-        GIT_TAG             release-1.8.0)
+        GIT_REPOSITORY      https://github.com/google/googletest.git)
     FetchContent_GetProperties(googletest)
     if(NOT googletest_POPULATED)
         FetchContent_Populate(googletest)
